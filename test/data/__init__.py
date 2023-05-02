4cc154e32e05fee23a4bf08f583b33c522461c48
Francesco Calcavecchia <f18771@eon.com>
2023-05-02 18:38:19 +0200

    minor cosmetic on test hack prompt and answer

--- test/data/__init__.py
+++ test/data/__init__.py
@@ -2,8 +2,8 @@ from pathlib import Path
 
 
 def read_hack_prompt() -> str:
-    return (Path(__file__).parent / "hack_prompt.txt").read_text()
+    return (Path(__file__).parent / "hack_prompt.txt").read_text()[:-1]
 
 
 def read_hack_prompt_answer() -> str:
-    return (Path(__file__).parent / "hack_prompt_answer.txt").read_text()
+    return (Path(__file__).parent / "hack_prompt_answer.txt").read_text()[:-1]
