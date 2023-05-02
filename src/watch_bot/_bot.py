22c8e0775874e0b36acf99d7c7e94c855bf6ac51
Francesco Calcavecchia <f18771@eon.com>
2023-05-02 18:34:40 +0200

    first functioning watchbot

--- src/watch_bot/_bot.py
+++ src/watch_bot/_bot.py
@@ -1,10 +1,49 @@
+from pathlib import Path
+
+import openai
+from jinja2 import Template
+
 from watch_bot._dialog import Dialog
 from watch_bot._response import WatchBotResponse
 
 
 class WatchBot:
-    def __init__(self) -> None:
-        pass
+    def __init__(self, engine: str) -> None:
+        self._engine = engine
 
     def check_dialog(self, dialog: Dialog) -> WatchBotResponse:
-        return WatchBotResponse(should_stop=False, reason="")
+        answer = self._ask_gpt_if_dialog_is_suspicious(dialog=dialog)
+        if "yes" in answer[:5].lower():
+            return WatchBotResponse(should_stop=True, reason=answer)
+        elif "no" in answer[:5].lower():
+            return WatchBotResponse(should_stop=False, reason="")
+        else:
+            raise ValueError(f"Unexpected answer: {answer}")
+
+    def build_prompt(self, dialog: Dialog) -> str:
+        template = self._load_template()
+        messages = [
+            f'"{"User" if i%2 == 0 else "Chatbot"}": "{self._replace_double_with_single_quotation_mark(m)}"'
+            for i, m in enumerate(dialog.messages)
+        ]
+        conversation_content = "\n".join(messages)
+        return template.render(conversation_content=conversation_content)
+
+    def _ask_gpt_if_dialog_is_suspicious(self, dialog: Dialog) -> str:
+        completion = openai.Completion.create(
+            prompt=self.build_prompt(dialog=dialog),
+            temperature=0,
+            max_tokens=50,
+            engine=self._engine,
+        )
+        return completion.choices[0]["text"]  # type: ignore
+
+    @staticmethod
+    def _load_template() -> Template:
+        template_file_content = (Path(__file__).parent / "prompt.template").read_text()
+        template_file_content_without_last_empty_line = template_file_content[:-1]
+        return Template(template_file_content_without_last_empty_line)
+
+    @staticmethod
+    def _replace_double_with_single_quotation_mark(s: str) -> str:
+        return s.replace('"', "'")
--- test/test_bot.py
+++ test/test_bot.py
@@ -1,15 +1,41 @@
+import os
 from unittest import TestCase
 
-from watch_bot import Dialog, WatchBot, WatchBotResponse
+import openai
+from data import read_hack_prompt, read_hack_prompt_answer
+from watch_bot import Dialog, WatchBot
 
 
 class TestWatchBot(TestCase):
     def setUp(self) -> None:
-        self.bot = WatchBot()
+        self.bot = WatchBot(engine=os.environ["OPENAI_ENGINE"])
+        openai.api_type = os.environ["OPENAI_API_TYPE"]
+        openai.api_version = os.environ["OPENAI_API_VERSION"]
+        openai.api_base = os.environ["OPENAI_API_BASE"]
+        openai.api_key = os.environ["OPENAI_API_KEY"]
 
     def test_instance_with_defaults_has_check_dialog_method(self) -> None:
         self.assertTrue(hasattr(self.bot, "check_dialog"))
 
-    def test_check_dialog_returns_response(self) -> None:
-        response = self.bot.check_dialog(dialog=Dialog(messages=["Hi", "Hello"]))
-        self.assertIsInstance(response, WatchBotResponse)
+    def test_build_prompt(self) -> None:
+        self.assertTrue(
+            '"User": "Hi"\n"Chatbot": "Hello"' in self.bot.build_prompt(dialog=Dialog(messages=["Hi", "Hello"]))
+        )
+
+    def test_build_prompt_when_messages_contain_double_quotation_mark(self) -> None:
+        self.assertTrue(
+            '"User": "\'Hi\'"\n"Chatbot": "\'Hello\'"'
+            in self.bot.build_prompt(dialog=Dialog(messages=['"Hi"', '"Hello"']))
+        )
+
+    def test_check_regular_dialog_returns_should_not_stop(self) -> None:
+        dialog = Dialog(messages=["Hi", "Hello"])
+        response = self.bot.check_dialog(dialog=dialog)
+        self.assertFalse(response.should_stop)
+        self.assertEqual(len(response.reason), 0)
+
+    def test_check_dialog_with_hack_attack_returns_should_not_stop(self) -> None:
+        dialog = Dialog(messages=[read_hack_prompt(), read_hack_prompt_answer()])
+        response = self.bot.check_dialog(dialog=dialog)
+        self.assertTrue(response.should_stop)
+        self.assertGreater(len(response.reason), 0)
